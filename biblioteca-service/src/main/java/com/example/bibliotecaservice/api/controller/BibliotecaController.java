d67aeb4b70bd40dddffd16c8767d8bc45e2f10c7
vlambo3 <contadora.godoy.vanina@gmail.com>
2023-03-09 01:06:57 -0300

    Fixed @EnableFeignClient

--- biblioteca-service/src/main/java/com/example/bibliotecaservice/api/controller/BibliotecaController.java
+++ biblioteca-service/src/main/java/com/example/bibliotecaservice/api/controller/BibliotecaController.java
@@ -1,10 +1,8 @@
 package com.example.bibliotecaservice.api.controller;
 
 import com.example.bibliotecaservice.api.client.IJuegosServiceClient;
-import com.example.bibliotecaservice.api.service.BibliotecaService;
 import com.example.bibliotecaservice.domain.model.Juegos;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +15,6 @@ import java.util.List;
 @RequestMapping("/biblioteca")
 @RestController
 public class BibliotecaController {
-
 
     private final IJuegosServiceClient iJuegosServiceClient;
 
